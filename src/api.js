4cf85478584de70d1e76da0fe1ee3280b926b20a
Tayyab Abdullah <tabdullah0801@gmail.com>
2023-07-21 16:05:11 +0500

    Added Code Funtionalities in all files including API keys and calls for services

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5c7df0f073mshc3808fe2c1c158bp15c005jsnaf03f8b3ecab',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ed855d05ac7557a90c5c05bf57395b6e";
